962d843ecd30644664311018fdb05e77287292e4
Pradithya Aria <pradithya.pura@go-jek.com>
2023-02-10 15:54:14 +0800

    Update local development setup

--- infra/local/dataset-service/setup.sh
+++ infra/local/dataset-service/setup.sh
@@ -5,17 +5,22 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-INGRESS_HOST=127.0.0.1
+INGRESS_HOST=127.0.0.1.nip.io
 CLUSTER_NAME=timber-dev
 MLP_CHART_VERSION=0.3.4
+MLP_URL=http://mlp.mlp.${INGRESS_HOST}
 TIMEOUT=600s
+PROJECT_NAME=testing
+
 
 setup_cluster() {
   echo "Setting up k3d cluster"
   if [[ $(k3d cluster list | grep $CLUSTER_NAME | wc -l) -eq 0 ]]
   then
-    k3d cluster create $CLUSTER_NAME --image rancher/k3s:v1.22.15-k3s1 --k3s-arg '--no-deploy=traefik,metrics-server@server:*' --port 80:80@loadbalancer
+    k3d cluster create $CLUSTER_NAME --image rancher/k3s:v1.22.15-k3s1 --k3s-arg 'metrics-server@server:*' --port 80:80@loadbalancer
   fi
+
+  k3d kubeconfig get ${CLUSTER_NAME} > /tmp/kubeconfig-${CLUSTER_NAME}.yaml
 }
 
 add_helm_repo() {
@@ -32,7 +37,7 @@ install_mlp() {
     --set fullnameOverride=mlp \
     --set deployment.apiHost=http://mlp.mlp.${INGRESS_HOST}/v1 \
     --set ingress.enabled=true \
-    --set ingress.class=traefik \
+    --set ingress.class="traefik" \
     --set ingress.host=mlp.mlp.${INGRESS_HOST} \
     --wait --timeout=${TIMEOUT}
 }
@@ -43,7 +48,17 @@ install_kafka() {
     --values kafka/values.yaml --wait --timeout=${TIMEOUT}
 }
 
+create_mlp_project() {
+  echo "Creating merlin project: PROJECT_NAME"
+  curl "${MLP_URL}/v1/projects" -d "{
+    \"name\"   : \"${PROJECT_NAME}\",
+    \"team\"   : \"dsp\",
+    \"stream\" : \"dsp\"
+  }"
+}
+
 add_helm_repo
 setup_cluster
 install_mlp
 install_kafka
+create_mlp_project
