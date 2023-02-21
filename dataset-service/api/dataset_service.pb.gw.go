ac8526e7793fd67361e70eb1597eeaf87830012c
Pradithya Aria <pradithya.pura@go-jek.com>
2023-02-21 17:13:34 +0800

    Update generated code and documentation

--- dataset-service/api/dataset_service.pb.gw.go
+++ dataset-service/api/dataset_service.pb.gw.go
@@ -155,6 +155,10 @@ func local_request_DatasetService_GetLogMetadata_0(ctx context.Context, marshale
 
 }
 
+var (
+	filter_DatasetService_ListLogWriters_0 = &utilities.DoubleArray{Encoding: map[string]int{"project_id": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
+)
+
 func request_DatasetService_ListLogWriters_0(ctx context.Context, marshaler runtime.Marshaler, client DatasetServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq ListLogWritersRequest
 	var metadata runtime.ServerMetadata
@@ -174,6 +178,13 @@ func request_DatasetService_ListLogWriters_0(ctx context.Context, marshaler runt
 	protoReq.ProjectId, err = runtime.Int64(val)
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "project_id", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_DatasetService_ListLogWriters_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := client.ListLogWriters(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
@@ -200,6 +211,13 @@ func local_request_DatasetService_ListLogWriters_0(ctx context.Context, marshale
 	protoReq.ProjectId, err = runtime.Int64(val)
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "project_id", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_DatasetService_ListLogWriters_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := server.ListLogWriters(ctx, &protoReq)
