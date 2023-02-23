9686bc4e2957201dd3a337918e311cb398881ff7
Pradithya Aria <pradithya.pura@go-jek.com>
2023-02-24 00:32:40 +0800

    Add observation service model converter

--- dataset-service/model/observation_service.go
+++ dataset-service/model/observation_service.go
@@ -3,10 +3,13 @@ package model
 import (
 	"database/sql/driver"
 
-	timberv1 "github.com/caraml-dev/timber/dataset-service/api"
 	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	timberv1 "github.com/caraml-dev/timber/dataset-service/api"
 )
 
+// ObservationService observation service internal model representation
 type ObservationService struct {
 	// Base provides common DB model field, namely ID, ProjectID, CreatedAt, UpdatedAt
 	Base
@@ -18,6 +21,34 @@ type ObservationService struct {
 	Status Status
 	// Error message
 	Error string `gorm:"size:2048"`
+}
+
+// ToObservationServiceProto convert internal ObservationService representation into ObservationService proto message
+func (w *ObservationService) ToObservationServiceProto() *timberv1.ObservationService {
+	return &timberv1.ObservationService{
+		ProjectId: w.ProjectID,
+		Id:        w.ID,
+		Name:      w.Name,
+		Source:    w.Source.ObservationServiceSource,
+		Status:    w.Status.ToStatusProto(),
+		Error:     w.Error,
+		CreatedAt: timestamppb.New(w.CreatedAt),
+		UpdatedAt: timestamppb.New(w.UpdatedAt),
+	}
+}
+
+// ObservationServiceFromProto convert ObservationService proto to internal representation of ObservationService
+func ObservationServiceFromProto(msg *timberv1.ObservationService) *ObservationService {
+	return &ObservationService{
+		Base: Base{
+			ID:        msg.Id,
+			ProjectID: msg.ProjectId,
+		},
+		Name:   msg.Name,
+		Source: &ObservationServiceSource{msg.Source},
+		Status: StatusFromProto(msg.Status),
+		Error:  msg.Error,
+	}
 }
 
 // ObservationServiceSource is wrapper of timberv1.ObservationServiceSource proto message to allow marshalling and unmarshalling to DB
