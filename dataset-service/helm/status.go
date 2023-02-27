5d454381c3368fb4ef57c3854bd2c3ad56f11b34
Pradithya Aria <pradithya.pura@go-jek.com>
2023-02-27 18:23:08 +0800

    Remove unused function

--- dataset-service/helm/status.go
+++ dataset-service/helm/status.go
@@ -3,7 +3,6 @@ package helm
 import (
 	"helm.sh/helm/v3/pkg/release"
 
-	"github.com/caraml-dev/timber/dataset-service/api"
 	"github.com/caraml-dev/timber/dataset-service/model"
 )
 
@@ -20,22 +19,5 @@ func ConvertStatus(status release.Status) model.Status {
 		return model.StatusPending
 	default:
 		return model.StatusUnspecified
-	}
-}
-
-// ConvertStatusToProto converts helm status to dataset service api status
-// TODO: remove
-func ConvertStatusToProto(status release.Status) api.Status {
-	switch status {
-	case release.StatusDeployed, release.StatusSuperseded:
-		return api.Status_STATUS_DEPLOYED
-	case release.StatusUninstalled:
-		return api.Status_STATUS_UNINSTALLED
-	case release.StatusFailed:
-		return api.Status_STATUS_FAILED
-	case release.StatusUninstalling, release.StatusPendingInstall, release.StatusPendingUpgrade, release.StatusPendingRollback:
-		return api.Status_STATUS_PENDING
-	default:
-		return api.Status_STATUS_UNSPECIFIED
 	}
 }
